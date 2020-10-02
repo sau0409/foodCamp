622216a490b2acf9181548369023c8e916adac90
sau0409 <64318068+sau0409@users.noreply.github.com>
2020-10-03 03:17:16 +0530

    cart overlay close

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 let cartBtn = document.getElementById("cartBtn");
+let cartOverlayCloseBtn = document.getElementById("cartOverlayCloseBtn");
 let cartoverlay = document.querySelector(".cart-overlay");
 
 
@@ -7,4 +8,11 @@ let cartoverlay = document.querySelector(".cart-overlay");
 cartBtn.addEventListener("click", ()=> {
     console.log("clciked");
     cartoverlay.classList.add("cart-overlay-show");
+    document.body.classList.add("black-overlay");
+});
+
+cartOverlayCloseBtn.addEventListener("click", ()=> {
+    console.log("clciked");
+    cartoverlay.classList.remove("cart-overlay-show");
+    document.body.classList.remove("black-overlay");
 });
