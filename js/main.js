64e1fc11317b8275444aed08ad6255eb4827d4d8
sau0409 <64318068+sau0409@users.noreply.github.com>
2020-10-03 02:46:35 +0530

    cart overlay

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+let cartBtn = document.getElementById("cartBtn");
+let cartoverlay = document.querySelector(".cart-overlay");
 
 
+// listen to cart click
+
+cartBtn.addEventListener("click", ()=> {
+    console.log("clciked");
+    cartoverlay.classList.add("cart-overlay-show");
+});
