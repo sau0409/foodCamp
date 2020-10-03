9cc1c75fc3509872c4dd9187135058a9b0a39ea1
sau0409 <64318068+sau0409@users.noreply.github.com>
2020-10-03 14:05:31 +0530

    variables added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
+// all button selected
+let barsBtn = document.getElementById("barsBtn");
 let cartBtn = document.getElementById("cartBtn");
+let cartPlus = document.getElementById("cartPlus");
+let cartMinus = document.getElementById("cartMinus");
+let checkoutBtn = document.getElementById("checkoutBtn");
+let addItemBtn = document.getElementById("addItemBtn");
 let cartOverlayCloseBtn = document.getElementById("cartOverlayCloseBtn");
+let cartOverlayResetBtn = document.getElementById("cartOverlayResetBtn");
 let cartoverlay = document.querySelector(".cart-overlay");
+let cartTotalCount = document.querySelector(".item-count-div");
+let cartItemCount = document.querySelector(".card-cart-count");
+let totalAmount = document.getElementById("totalAmout");
 
 
-// listen to cart click
+// listen to navigation cart button click to open overlay
 
-cartBtn.addEventListener("click", ()=> {
+cartBtn.addEventListener("click", () => {
     console.log("clciked");
     cartoverlay.classList.add("cart-overlay-show");
     document.body.classList.add("black-overlay");
 });
 
-cartOverlayCloseBtn.addEventListener("click", ()=> {
+// listen to cart c;ose button click to close overlay
+
+cartOverlayCloseBtn.addEventListener("click", () => {
     console.log("clciked");
     cartoverlay.classList.remove("cart-overlay-show");
     document.body.classList.remove("black-overlay");
 });
+
+// cart count
