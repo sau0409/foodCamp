2feb074b4885493d92cad4084b802cfc2b57ddd0
sau0409 <64318068+sau0409@users.noreply.github.com>
2020-10-04 19:08:02 +0530

    added reset functionalities

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const barsBtn = document.getElementById("barsBtn");
 const cartBtn = document.getElementById("cartBtn");
 const checkoutBtn = document.getElementById("checkoutBtn");
 const cartOverlayCloseBtn = document.getElementById("cartOverlayCloseBtn");
-const cartOverlayResetBtn = document.getElementById("cartOverlayResetBtn");
+const cartResetBtn = document.getElementById("cartOverlayResetBtn");
 const cartoverlay = document.querySelector(".cart-overlay");
 const cartTotalCount = document.querySelector(".item-count-div");
 const totalAmount = document.getElementById("totalAmout");
@@ -17,6 +17,8 @@ const cartItemCount = document.querySelector(".card-cart-count");
 // variables
 
 let cart = [];
+
+let productAddBtn = [];
 
 // getting products
 
@@ -58,6 +60,7 @@ class Products {
 class Ui {
 
     displayProducts(products) {
+        //will display all the products
         products.forEach(product => {
             let productDiv = `<div class="col-sm">
             <div class="card product-card">
@@ -71,14 +74,15 @@ class Ui {
                 </div>
             </div>
         </div>`
+            //adding products in container
             productItemCntainer.insertAdjacentHTML('afterbegin', productDiv);
-            
+
         });
     }
 
     getProductAddBtn() {
         const addItemBtn = [...document.querySelectorAll(".product-add-btn")];
-
+        productAddBtn = addItemBtn;
         addItemBtn.forEach((el, index) => {
             let btnId = el.getAttribute("id");
             if (cart.find((ele) =>
@@ -86,7 +90,7 @@ class Ui {
                 )) {
                 el.innerHTML = "In cart";
                 el.disabled = true;
-            }
+            } 
             el.addEventListener("click", () => {
                 let product = Storage.getProduct(btnId);
                 //destructuring product as per need 
@@ -105,17 +109,19 @@ class Ui {
                 el.innerHTML = "In cart";
                 el.disabled = true;
                 // display cart length on navbar
-                
+
                 this.setCartValues(cart);
-                console.log(cartItem);
                 this.displayCartItem(cartItem);
+
             })
-            let cartCount = 0;
-            cart.forEach((elem)=> {
-                cartCount+= elem.itemCount;
-            })
-            cartTotalCount.innerHTML = String(cartCount);
+
+            this.setCartValues(cart);
+
         });
+
+        cart.forEach((cartItem) => {
+            this.displayCartItem(cartItem);
+        })
     }
 
     //calculation total cart amount and total item count
@@ -123,9 +129,9 @@ class Ui {
     setCartValues(cart) {
         let cartCount = 0;
         let cartAmount = 0;
-        cart.forEach((elem)=> {
-            cartCount+= elem.itemCount;
-            cartAmount+= elem.itemCount * parseInt(elem.price);
+        cart.forEach((elem) => {
+            cartCount += elem.itemCount;
+            cartAmount += elem.itemCount * parseInt(elem.price);
         })
         cartTotalCount.innerHTML = String(cartCount);
         totalAmount.innerText = String(cartAmount);
@@ -134,31 +140,135 @@ class Ui {
     //displaying cart
 
     displayCartItem(item) {
-        console.log(item);
-            let cartItemDiv = `<div class="card card-cart">
+        let cartItemDiv = `<div class="card card-cart" id="item-${item.itemId}">
             <div>
                 <img src="${item.img}" class="cart-img" alt="kdai paneer image">
             </div>
             <div class="card-cart-t">
                 <p class="card-cart-t-1">${item.itemName}</p>
-                <p class="card-cart-t-2">${item.price} &#x20B9;</p>
+                <p class="card-cart-t-2">${item.price  * item.itemCount} &#x20B9;</p>
             </div>
             <div class="cart-cal-item">
-                <button class="cart-btn" data-id=${item.itemId}>
+                <button class="cart-btn" data-id=${item.itemId} functionality="add">
                     <i class="fas fa-plus-square cart-ic"></i>
                 </button>
                 <div class="card-cart-count">${item.itemCount}</div>
-                <button class="cart-btn" data-id=${item.itemId}>
+                <button class="cart-btn" data-id=${item.itemId} functionality="minus">
                     <i class="fas fa-minus-square cart-ic"></i>
                 </button>
             </div>
         </div>`
-        console.log(cartItemDiv);
         cartItemContainer.insertAdjacentHTML("afterbegin", cartItemDiv);
 
     }
 
+    //setup initial cart
 
+    setupCart() {
+
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+
+        // listen to navigation cart button click to open overlay
+
+        cartBtn.addEventListener("click", () => {
+            cartoverlay.classList.add("cart-overlay-show");
+            document.body.classList.add("black-overlay");
+        });
+
+        // listen to cart close button click to close overlay
+
+        cartOverlayCloseBtn.addEventListener("click", () => {
+            cartoverlay.classList.remove("cart-overlay-show");
+            document.body.classList.remove("black-overlay");
+        });
+    }
+
+    populateCart(cart) {
+        cart.forEach((el) => this.displayCartItem(el));
+    }
+
+
+    cartLogic() {
+
+        cartResetBtn.addEventListener("click", () => {
+            /*cart = []; // cart to null
+            Storage.storeCart(cart); //resetting localStorage
+            cartItemContainer.innerHTML = ''; // cart items to null
+            ui.setCartValues(cart); // setting cart values*/
+            this.clearCart();
+        });
+
+
+
+        /*const cardBtn = document.querySelectorAll('.cart-btn');
+        cardBtn.forEach((card) => {
+            card.addEventListener("click", () => {
+                console.log("cart btn pressed")
+                let cartItemPressed = cart.find((el) => {
+                    return el.itemId === card.getAttribute("data-id");
+                })
+                console.log(cartItemPressed);
+                let functionality = card.getAttribute("functionality");
+                console.log(functionality);
+
+                if (functionality === 'add') {
+                    console.log("cart add btn pressed")
+                    cartItemPressed.itemCount = parseInt(cartItemPressed.itemCount) + 1;
+                    this.removeCartItem(cartItemPressed.itemId);
+                    console.log("cart item removed");
+                    this.displayCartItem(cartItemPressed);
+                    console.log("cart item added");
+                } else if (functionality === 'minus') {
+                    if (parseInt(cartItemPressed.itemCount) > 1) {
+                        console.log("cart minus btn pressed")
+                        cartItemPressed.itemCount = parseInt(cartItemPressed.itemCount) - 1;
+                        this.removeCartItem(cartItemPressed.itemId);
+                        this.displayCartItem(cartItemPressed);
+                        console.log("cart item added");
+                    } else {
+                        console.log("cart minus btn pressed else logic")
+                        let itemIndex = cart.findIndex((el) => {
+                            return el.itemId === cartItemPressed.itemId;
+                        });
+                        cart.splice(itemIndex, 1);
+                        this.removeCartItem(cartItemPressed.itemId);
+                    }
+                }
+
+                Storage.storeCart(cart);
+                this.updateCart(cart);
+            })
+        })*/
+    }
+
+    clearCart() {
+        let cartItems = cart.map((item)=> item.itemId);
+        cartItems.forEach((el)=> {
+            this.removeCartItem(el);
+        });
+        console.log(cartItems);
+    }
+
+    removeCartItem(itemId) {
+        cart = cart.filter((item)=> {
+            return item.itemId !== itemId;
+        });
+        this.setCartValues(cart);
+        Storage.storeCart(cart);
+        let itemEl = document.getElementById(`item-${itemId}`);
+        cartItemContainer.removeChild(itemEl);
+        this.enableProduct(itemId);
+    }
+
+    enableProduct(itemId) {
+        let product = productAddBtn.find((el)=> {
+            return itemId === el.getAttribute("id");
+        })
+        product.innerHTML = "Add";
+        product.disabled = false;
+    }
 
 }
 
@@ -168,14 +278,14 @@ class Storage {
     static storeProducts(products) {
         window.sessionStorage.setItem("products", JSON.stringify(products));
     }
-    
+
     static getProducts() {
         return JSON.parse(window.sessionStorage.getItem("products"));
     }
 
     static getProduct(id) {
         let productsArray = this.getProducts();
-        return productsArray.find((elem)=> {
+        return productsArray.find((elem) => {
             return elem.itemId === id
         })
     }
@@ -185,12 +295,12 @@ class Storage {
     }
 
     static getCart() {
-        return JSON.parse(window.localStorage.getItem("cart"));
+        return localStorage.getItem("cart") ? JSON.parse(window.localStorage.getItem("cart")) : [];
     }
 
     static getCartItem(id) {
         let cartArray = this.getCart();
-        return cartArray.find((elem)=> {
+        return cartArray.find((elem) => {
             return elem.itemId === id
         })
     }
@@ -200,36 +310,28 @@ class Storage {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    let cartLocalStorage = JSON.parse(window.localStorage.getItem("cart"));
-        if(cartLocalStorage) {
-            cart = cartLocalStorage;
-        }
-
-    // listen to navigation cart button click to open overlay
-
-    cartBtn.addEventListener("click", () => {
-        cartoverlay.classList.add("cart-overlay-show");
-        document.body.classList.add("black-overlay");
-    });
-
-    // listen to cart close button click to close overlay
-
-    cartOverlayCloseBtn.addEventListener("click", () => {
-        cartoverlay.classList.remove("cart-overlay-show");
-        document.body.classList.remove("black-overlay");
-    });
-
     // creating instance of classes
 
     let products = new Products();
     let ui = new Ui();
+
+    //setup cart items
+    ui.setupCart();
+    /*
+        let cartLocalStorage = JSON.parse(window.localStorage.getItem("cart"));
+        if (cartLocalStorage) {
+            cart = cartLocalStorage;
+        }
+    */
+
+
 
     products.getProductsFromBackend().then((products) => {
         ui.displayProducts(products);
         Storage.storeProducts(products);
     }).then(() => {
         ui.getProductAddBtn();
-        
+        ui.cartLogic();
     });
 
 
